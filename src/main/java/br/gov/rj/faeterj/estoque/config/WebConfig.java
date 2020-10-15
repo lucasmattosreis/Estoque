8caf17e57f44169c37f0c1d46b14f9f1637fa01a
Lucas Reis <lucasmattos68@gmail.com>
2020-10-15 10:43:33 -0300

    Projeto Estoque

--- src/main/java/br/gov/rj/faeterj/estoque/config/WebConfig.java
+++ src/main/java/br/gov/rj/faeterj/estoque/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -69,4 +70,13 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		// (4) Definição do modo do template que será usado
 		return resolver;
 	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// Permite registrar recursos estáticos a aplicação
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		
+		
+	}
+	
 }
